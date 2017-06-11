a0b3ad11fcd5ce0fabb1b34d862ded0e09c260bf
Andrei Vasilevich <a.vasilevich@gamesbannernet.com>
2017-06-12 01:07:17 +0600

    Initial

---
+++ src/main/kotlin/com/avp/studio/KtStudioReactiveRestServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.avp.studio
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KtStudioReactiveRestServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KtStudioReactiveRestServiceApplication::class.java, *args)
+}
